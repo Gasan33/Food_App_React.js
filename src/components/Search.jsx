206e3531ffb2dde4377ea8b142ba010cd2e80004
ALFAFA <gasan1288@gmail.com>
2024-09-30 22:14:47 +0400

    Food List & Food Item Add

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "0ff63be07d214ee98c895f92bdce100b";
-export default function Search() {
+export default function Search({ foodData, setFoodData }) {
     const [query, setQuery] = useState("pizza");
     useEffect(() => {
         async function fetchFood() {
             const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
             const data = await res.json();
             console.log(data.results);
+            setFoodData(data.results);
         }
         fetchFood();
     }, [query]);
