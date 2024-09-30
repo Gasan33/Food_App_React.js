fd78e49019a00667a7f876bbc05ab872de427207
ALFAFA <gasan1288@gmail.com>
2024-09-30 23:03:12 +0400

    Create Screen Container & inner Container And align Food Items on the left

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Search from './components/Search'
 import FoodList from './components/FoodList';
 import Nav from './components/Nav';
 import "./App.css";
+import Container from './components/Container';
+import InnerComponent from './components/InnerContainer';
+import InnerContainer from './components/InnerContainer';
 
 function App() {
   const [foodData, setFoodData] = useState([]);
@@ -11,7 +14,12 @@ function App() {
     <div className="App">
       <Nav />
       <Search foodData={foodData} setFoodData={setFoodData} />
-      <FoodList foodData={foodData} />
+      <Container>
+        <InnerContainer>
+          <FoodList foodData={foodData} />
+
+        </InnerContainer>
+      </Container>
     </div>
   )
 }
