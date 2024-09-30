fb7f5f03a2255f7d9728e4010cc810238b803d00
ALFAFA <gasan1288@gmail.com>
2024-09-30 22:33:35 +0400

    Styling The Search Component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import styles from "./search.module.css";
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "0ff63be07d214ee98c895f92bdce100b";
 export default function Search({ foodData, setFoodData }) {
@@ -13,8 +14,8 @@ export default function Search({ foodData, setFoodData }) {
         fetchFood();
     }, [query]);
     return (
-        <div>
-            <input value={query} onChange={(e) => setQuery(e.target.value)} type="text" />
+        <div className={styles.searchContainer}>
+            <input className={styles.input} value={query} onChange={(e) => setQuery(e.target.value)} type="text" />
         </div>
     );
 }
